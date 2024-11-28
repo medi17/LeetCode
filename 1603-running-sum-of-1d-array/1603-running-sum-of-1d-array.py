8f5fd1474b31ab88745317e8118a45a8d7a197fb
Medan <medanabebe17@gmail.com>
2024-11-28 13:47:46 +0300

    Time: 0 ms (100%), Space: 12.4 MB (5.91%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -1,16 +1,8 @@
 class Solution(object):
     def runningSum(self, nums):
-        
-        sums = nums[0]
-        for i in  range(1, len(nums)):
-            sums += nums[i]
-            nums[i] = sums
-            
+        for i in range(1,len(nums)):
+            nums[i]+=nums[i-1]
         return nums
-
-
-
-
 
         """
         :type nums: List[int]
