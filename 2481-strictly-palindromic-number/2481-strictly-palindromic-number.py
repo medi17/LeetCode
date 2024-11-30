b3443b0c13ae40314b31b10edaaed24f2389ae29
Medan <medanabebe17@gmail.com>
2024-11-30 22:44:34 +0300

    Time: 0 ms (100%), Space: 12 MB (34%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -1,19 +1,20 @@
 class Solution(object):
     def isStrictlyPalindromic(self, n):
-        
-        for i in range(2, n-1):
-            s = ''
-            q = n
-            while q > 0:
-                r = q % i
-                q = q // i
+        return False
+
+        # for i in range(2, n-1):
+        #     s = ''
+        #     q = n
+        #     while q > 0:
+        #         r = q % i
+        #         q = q // i
             
-                s += str(r)
+        #         s += str(r)
            
 
-            if s != s[::-1]:
-                return False
-        return True
+        #     if s != s[::-1]:
+        #         return False
+        # return True
 
         """
         :type n: int
