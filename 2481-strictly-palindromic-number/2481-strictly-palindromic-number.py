168ab1c9a09aeb5bf9382b1c36f0a3e566399485
Medan <medanabebe17@gmail.com>
2024-11-30 22:42:25 +0300

    Time: 3 ms (25%), Space: 15.3 MB (15.5%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        
+        for i in range(2, n-1):
+            s = ''
+            q = n
+            while q > 0:
+                r = q % i
+                q = q // i
+            
+                s += str(r)
+           
+
+            if s != s[::-1]:
+                return False
+        return True
+
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
